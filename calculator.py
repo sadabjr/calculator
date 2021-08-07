a5ebd1f0d501445d493b85b51f12b276aa56b9ce
Sadab Ali <84293105+sadabjr@users.noreply.github.com>
2021-08-07 21:56:08 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,67 @@
+#calculator
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+print(logo)
+
+
+#add
+def add(n1, n2):
+  return n1 + n2
+
+#Subtract
+def subtract(n1, n2):
+  return n1 - n2
+
+#Multiply
+def multiply(n1, n2):
+  return n1 * n2
+
+#Divide
+def divide(n1, n2):
+  return n1 / n2
+
+oprations = {"+" : add,
+ "-" : subtract,
+  "*" : multiply,
+   "/" : divide
+   }
+
+def calculator():
+    num1 = float(input("Whats the first number? :" ))
+
+    for symbol in oprations:
+        print(symbol)
+    should_continue = True
+
+
+    while should_continue:
+        opration_symbol =input("Pick an opration :")
+        num2 = float(input("What's the next number: "))
+        calculation_function = oprations[opration_symbol]
+
+        answer = calculation_function(num1, num2)
+
+        print(f"{num1} {opration_symbol} {num2} = {answer}")
+
+        if input(f"Type 'y' to continue calcutating with {answer} or type 'n' to start anew calculation:" ) == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+calculator()
